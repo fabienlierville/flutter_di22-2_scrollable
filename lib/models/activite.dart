b34d11f99c548eb753e9d7681a17dd50e25900b1
fabien <flierville@cesi.fr>
2024-01-22 09:35:47 +0100

    ListView avec Container

---
+++ lib/models/activite.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class Activite{
+  String nom;
+  IconData icone;
+
+  Activite({
+    required this.nom,
+    required this.icone,
+  });
+}
---
+++ lib/pages/list_view_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:scrollable/models/activite.dart';
+
+class ListViewPage extends StatefulWidget {
+  const ListViewPage({super.key});
+
+  @override
+  State<ListViewPage> createState() => _ListViewPageState();
+}
+
+class _ListViewPageState extends State<ListViewPage> {
+  List<Activite> activites = [
+    Activite(nom: "Vélo", icone: Icons.directions_bike),
+    Activite(nom: "Peinture", icone: Icons.palette),
+    Activite(nom: "Golf", icone: Icons.golf_course),
+    Activite(nom: "Jeux vidéo", icone: Icons.gamepad),
+    Activite(nom: "Bricolage", icone: Icons.build),
+    Activite(nom: "Vélo", icone: Icons.directions_bike),
+    Activite(nom: "Peinture", icone: Icons.palette),
+    Activite(nom: "Golf", icone: Icons.golf_course),
+    Activite(nom: "Jeux vidéo", icone: Icons.gamepad),
+    Activite(nom: "Bricolage", icone: Icons.build),
+    Activite(nom: "Vélo", icone: Icons.directions_bike),
+    Activite(nom: "Peinture", icone: Icons.palette),
+    Activite(nom: "Golf", icone: Icons.golf_course),
+    Activite(nom: "Jeux vidéo", icone: Icons.gamepad),
+    Activite(nom: "Bricolage", icone: Icons.build),
+    Activite(nom: "Vélo", icone: Icons.directions_bike),
+    Activite(nom: "Peinture", icone: Icons.palette),
+    Activite(nom: "Golf", icone: Icons.golf_course),
+    Activite(nom: "Jeux vidéo", icone: Icons.gamepad),
+    Activite(nom: "Bricolage", icone: Icons.build),
+    Activite(nom: "Vélo", icone: Icons.directions_bike),
+    Activite(nom: "Peinture", icone: Icons.palette),
+    Activite(nom: "Golf", icone: Icons.golf_course),
+    Activite(nom: "Jeux vidéo", icone: Icons.gamepad),
+    Activite(nom: "Bricolage", icone: Icons.build),
+    Activite(nom: "Vélo", icone: Icons.directions_bike),
+    Activite(nom: "Peinture", icone: Icons.palette),
+    Activite(nom: "Golf", icone: Icons.golf_course),
+    Activite(nom: "Jeux vidéo", icone: Icons.gamepad),
+    Activite(nom: "Bricolage", icone: Icons.build),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("List View"),),
+      body: ListView.builder(
+          itemCount: activites.length,
+          itemBuilder: (context, index){
+            return Row(
+              children: [
+                Container(height: 100,child: Text("Activité : ${activites[index].nom}"),),
+                Container(height: 100,child: Icon(activites[index].icone),)
+              ],
+            );
+          }
+      ),
+    );
+  }
+}
