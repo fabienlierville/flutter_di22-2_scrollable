e42d080e524e740006ac4b50e7149927b971e2de
fabien <flierville@cesi.fr>
2024-01-22 09:19:01 +0100

    SingleChildScrollView

--- lib/pages/single_child_page.dart
+++ lib/pages/single_child_page.dart
@@ -5,12 +5,27 @@ class SingleChildPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(title: Text("Single Child"),),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(height: 100,color: Colors.red,)
+            Container(height: 100,color: Colors.red,),
+            Container(height: 100,color: Colors.green,),
+            Container(height: 100,color: Colors.yellow,),
+            Container(height: 100,color: Colors.blue,),
+            Container(height: 100,color: Colors.black,),
+            Container(height: 100,color: Colors.red,),
+            Container(height: 100,color: Colors.green,),
+            Container(height: 100,color: Colors.yellow,),
+            Container(height: 100,color: Colors.blue,),
+            Container(height: 100,color: Colors.black,),
+            Container(height: 100,color: Colors.red,),
+            Container(height: 100,color: Colors.green,),
+            Container(height: 100,color: Colors.yellow,),
+            Container(height: 100,color: Colors.blue,),
+            Container(height: 100,color: Colors.black,),
           ],
         ),
       ),
